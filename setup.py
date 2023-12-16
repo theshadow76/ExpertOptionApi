b75c9be8a56f2e758417baa76c939350fa740ef2
Vigo Walker <vigopaul05@gmail.com>
2023-12-16 14:35:55 -0300

    update 982ujd3d

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='ExpertOptionAPI',
-    version='0.4',
+    version='0.5',
     packages=find_packages(where="./"),
     package_dir={'': "./"},
     long_description=long_description,
